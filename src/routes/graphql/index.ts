4bd06c1ce60824caabc4c6725a4637cf5c31b096
Nikolay <kolyak770@gmail.com>
2024-11-03 21:37:06 +0300

    init: initial commit

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,12 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, GraphQLObjectType, GraphQLSchema, parse, validate } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
+import { MemberTypesQuery } from './member-types/queries.js';
+import { PostsQuery } from './posts/queries.js';
+import { ProfilesQuery } from './profiles/queries.js';
+import { StatsQuery } from './stats/queries.js';
+import { UsersQuery } from './users/queries.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -15,7 +21,36 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      // return graphql();
+      const reqQuery = String(req.body.query);
+
+      const query = new GraphQLObjectType({
+        name: 'Query',
+        fields: () => ({
+          ...MemberTypesQuery,
+          ...PostsQuery,
+          ...ProfilesQuery,
+          ...StatsQuery,
+          ...UsersQuery,
+        }),
+      });
+
+      const mutation = new GraphQLObjectType({
+        name: 'Mutation',
+        fields: () => ({}),
+      });
+
+      const schema = new GraphQLSchema({ query, mutation });
+
+      const errors = validate(schema, parse(reqQuery), [depthLimit(5)]);
+
+      if (errors.length) return { errors };
+
+      return await graphql({
+        schema,
+        source: reqQuery,
+        variableValues: req.body.variables,
+        contextValue: { db: prisma },
+      });
     },
   });
 };
